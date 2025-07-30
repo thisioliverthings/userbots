19b81cee7cd7b30e99e7071922a0a90dd0f62704
thisioliverthings <Abodeking19999@gmail.com>
2025-07-30 21:00:23 +0300

    Update autoscroll.py

--- userbot/plugins/autoscroll.py
+++ userbot/plugins/autoscroll.py
@@ -4,35 +4,32 @@ from pyrogram.types import Message
 from userbot import UserBot
 from userbot.plugins.help import add_command_help
 
-the_regex = r"^r\/([^\s\/])+"
+# قائمة الدردشات المفعّل فيها التمرير التلقائي
+auto_read_chats = set()
 
-f = filters.chat([])
 
-
-@UserBot.on_message(f)
+@UserBot.on_message(filters.chat(auto_read_chats))
 async def auto_read(bot: UserBot, message: Message):
-    await UserBot.read_history(message.chat.id)
+    await bot.read_history(message.chat.id)
     message.continue_propagation()
 
 
-@UserBot.on_message(filters.command("autoscroll", ".") & filters.me)
-async def add_to_auto_read(bot: UserBot, message: Message):
-    if message.chat.id in f:
-        f.remove(message.chat.id)
-        await message.edit("Autoscroll deactivated")
+@UserBot.on_message(filters.command(["autoscroll", "تمرير"], ".") & filters.me)
+async def toggle_autoscroll(bot: UserBot, message: Message):
+    chat_id = message.chat.id
+
+    if chat_id in auto_read_chats:
+        auto_read_chats.remove(chat_id)
+        await message.edit("تم إيقاف التمرير التلقائي في هذه المحادثة.")
     else:
-        f.add(message.chat.id)
-        await message.edit("Autoscroll activated")
+        auto_read_chats.add(chat_id)
+        await message.edit("تم تفعيل التمرير التلقائي في هذه المحادثة.")
 
 
-# Command help section
+# قسم المساعدة - عربي
 add_command_help(
-    "autoscroll",
+    "التمرير التلقائي",
     [
-        [
-            ".autoscroll",
-            "Send .autoscroll in any chat to automatically read all sent messages until you call "
-            "autoscroll again. This is useful if you have Telegram open on another screen.",
-        ],
+        [".autoscroll / .تمرير", "تفعيل أو إيقاف قراءة الرسائل تلقائيًا في المحادثة الحالية."],
     ],
 )
